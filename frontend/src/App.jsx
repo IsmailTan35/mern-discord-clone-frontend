0ccb7789aeecf59f3c7b549d0cbc579aec47d368
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-01-07 16:35:34 +0300

    for vercel deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import Main from 'layouts/Main';
 import Panel from 'layouts/Panel';
 
-
 import SocketController from 'controller/SocketController';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
 const App = () => {
   return (
     <>
      
        {/* <SocketController/> */}
-      <Panel/>
+       <Switch>
+          <Route exact path="/" component={Panel}/>
+      </Switch>
     
     </> 
   );
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
-import App from './App';
+import App from 'App';
 import store from 'store';
 
 import 'assets/style/index.css';
