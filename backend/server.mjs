a59be35de9decfb6708365b4e2db4b12f4ebe03c
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-01-13 16:25:15 +0300

    for vercel

--- backend/server.mjs
+++ backend/server.mjs
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 // import con  from'./app_server/models/db.mjs'
 import express from"./app_server/helper/helperExpress.mjs"
 import { 
-    httpsServer, 
+    httpServer, 
     webSocket
 } from'./app_server/helper/helperProtocol.mjs'
 import controllerApi from'./app_server/controller/controllerApi.mjs'
@@ -10,7 +10,7 @@ import controlleWebsocket from'./app_server/controller/controllerWebsocket.mjs'
 
 const env = dotenv.config()
 const app = express()
-const server = httpsServer(app,10000)
+const server = httpServer(app,443)
 const socket = webSocket(server,app)
 
 controllerApi(app,"con")
