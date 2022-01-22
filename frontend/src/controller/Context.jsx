10229f039087123186155b094e10e6c857aa2e39
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-01-23 00:55:58 +0300

    change url

--- frontend/src/controller/Context.jsx
+++ frontend/src/controller/Context.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { io } from "socket.io-client";
 const protocol = window.location.protocol
-export const client = io.connect(`${protocol}//localhost:10000`,{
+const dev=`${protocol}//localhost:10000`
+const product=`https://discordbackend.herokuapp.com/`
+
+export const client = io.connect(product,{
     'reconnection': true,
     'reconnectionDelay': 2500,
     'reconnectionAttempts': 10
