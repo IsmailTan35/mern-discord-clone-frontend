c5353c8370775f1bee9c13a7fad67e11a0fe6f15
İsmail Tan <46776641+IsmailTan35@users.noreply.github.com>
2022-04-16 10:29:58 +0300

    url check

--- frontend/src/controller/Context.jsx
+++ frontend/src/controller/Context.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const protocol = window.location.protocol
 console.log(process.env.REACT_APP_URL_PRODUCTION);
 console.log(protocol)
-export const client = io.connect(protocol=="https"?process.env.REACT_APP_URL_PRODUCTION:process.env.REACT_APP_URL_DEV,{
+export const client = io.connect(protocol=="https:"?process.env.REACT_APP_URL_PRODUCTION:process.env.REACT_APP_URL_DEV,{
     'reconnection': true,
     'reconnectionDelay': 2500,
     'reconnectionAttempts': 10
