6296e06843a4bbf84bfe8e853ad1c1f3a44096ff
İsmail Tan <46776641+IsmailTan35@users.noreply.github.com>
2022-04-16 10:34:38 +0300

    url check

--- frontend/src/controller/Context.jsx
+++ frontend/src/controller/Context.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { io } from "socket.io-client";
-const protocol = window.location.protocol
-console.log(process.env.REACT_APP_URL_PRODUCTION);
-console.log(protocol)
-export const client = io.connect(protocol=="https:"?process.env.REACT_APP_URL_PRODUCTION:process.env.REACT_APP_URL_DEV,{
+const host = window.location.hostname
+
+export const url = host!="localhost"?process.env.REACT_APP_URL_PRODUCTION:process.env.REACT_APP_URL_DEV
+export const client = io.connect(url.replace("http://","").replace("https://","").replace("/",""),{
     'reconnection': true,
     'reconnectionDelay': 2500,
     'reconnectionAttempts': 10
   });
-
 export const SocketContext = React.createContext();
