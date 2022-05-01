7d997e5e41f6f19276cdb6fe38785512fb3ed70f
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-05-01 15:51:25 +0300

    ssl

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app)=> {
-    app.use(proxy('/api/socket', { target: 'ws://' + process.env.REACT_APP_URL_NAME, ws: true }));
-    app.use(proxy('/api', { target: 'http://' + process.env.REACT_APP_URL_NAME ,changeOrigin: true,secure:true}));
+    app.use(proxy('/api/socket', { target: 'ws://' + process.env.REACT_APP_URL_DEV, ws: true }));
+    app.use(proxy('/api', { target: 'http://' + process.env.REACT_APP_URL_DEV ,changeOrigin: true,secure:true}));
 };
