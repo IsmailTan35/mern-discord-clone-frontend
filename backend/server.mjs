157b4afd86c8813dfdb4e1d33e1fe34dd04f6b6b
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-05-01 15:29:02 +0300

    ssl

--- backend/server.mjs
+++ backend/server.mjs
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import express from"./app_server/helper/helperExpress.mjs"
 import { 
     // httpsServer,
-    httpServer, 
+    httpsServer, 
     webSocket
 } from'./app_server/helper/helperProtocol.mjs'
 import controllerApi from'./app_server/controller/controllerApi.mjs'
