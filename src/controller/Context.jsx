cf5b339385f6072e2cdf4e9c3780973ce25880bd
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-07-22 09:55:07 +0300

    url

--- src/controller/Context.jsx
+++ src/controller/Context.jsx
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const parsed = window.location.hostname.split(".")
 const protocol = window.location.protocol
 export const url = parsed.includes("vercel") ? process.env.REACT_APP_URL_PRODUCTION: `${protocol}//localhost:10000`
-
+console.log(parsed.includes("vercel"))
 export const client = io.connect(url.replace("http://","").replace("https://","").replace("/",""),{
     'reconnection': true,
     'reconnectionDelay': 2500,
