f5d49a2fa48a34ff0115db001848f89864338a99
İsmail Tan <46776641+IsmailTan35@users.noreply.github.com>
2022-11-30 21:37:58 +0300

    vercel

--- server.ts
+++ server.ts
@@ -4,17 +4,17 @@ import {
     CreateServer,
     webSocket
 } from'./backend/app_server/helper/helperProtocol'
-// import controllerApi from'./app_server/controller/apicontroller/index.mjs'
-// import controlleWebsocket from'./app_server/controller/socketcontroller/index.mjs'
-// import mongoDb from"./app_server/models/mongoDb.mjs"
+import controllerApi from'./app_server/controller/apicontroller/index.mjs'
+import controlleWebsocket from'./app_server/controller/socketcontroller/index.mjs'
+import mongoDb from"./app_server/models/mongoDb.mjs"
 
 const env = dotenv.config()
 const app = express()
 const port = process.env.PORT || 10000 
-// const db = mongoDb()
+const db = mongoDb()
 const server = CreateServer(app,port)
-// const socket = webSocket(server)
-// app.io=socket
+const socket = webSocket(server)
+app.io=socket
 
-// controllerApi(app,"con")
-// controlleWebsocket(socket,"con")
+controllerApi(app,"con")
+controlleWebsocket(socket,"con")
