ac9bdbfbafabf4edacee8b88bbdee052ae6ee2e7
IsmailTan35 <tan_tan.2000@hotmail.com>
2022-12-01 00:21:51 +0300

    vercel

--- server2.ts
+++ server2.ts
@@ -1,26 +1,9 @@
-import {createServer as httpCreate} from "http";
 
-const httpServer = (app:any,port:any) =>{
-	const httpServer = httpCreate(app)
-	httpServer.listen(port, () =>{
-		console.info((new Date()) + ' Http server is listening on port ' + port);
-	});
-	return httpServer
-}
 
 import express from 'express'
-import bodyParser from 'body-parser'
+const app = express();
 
-import cors from 'cors'
-
-    const app = express();
-    // app.use(cors)
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(express.json())
-    app.use(cors())
-    // app.use(apiRequestLimiter)
-    app.get("/",(req:any,res:any)=>{
-        res.send("sadas")
-    })
-const server = httpServer(app,10000)
+app.get("/",(req:any,res:any)=>{
+    res.send("sadas")
+})
+app.listen(3000)
