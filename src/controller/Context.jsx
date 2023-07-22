f269902aa4ebc6a1297f80d2843fbe7ac2be3761
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-07-22 18:18:00 +0300

    changed socket url

--- src/controller/Context.jsx
+++ src/controller/Context.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { io } from "socket.io-client";
 const hostname = window.location.hostname;
-const parsed = window.location.hostname.split(".")
-const protocol = window.location.protocol
-export const url = parsed.includes("vercel") ? process.env.REACT_APP_URL_PRODUCTION: `${protocol}//${hostname}:10000`
+const parsed = window.location.hostname.split(".");
+const protocol = window.location.protocol;
+export const url = parsed.includes("ismailtan")
+  ? process.env.REACT_APP_URL_PRODUCTION
+  : `${protocol}//${hostname}:10000`;
 
-export const client = io.connect(url.replace("http://","").replace("https://","").replace("/",""),{
-    'reconnection': true,
-    'reconnectionDelay': 2500,
-    'reconnectionAttempts': 10,
-    'forceNew':true
-  });
+export const client = io.connect(
+  url.replace("http://", "").replace("https://", "").replace("/", ""),
+  {
+    reconnection: true,
+    reconnectionDelay: 2500,
+    reconnectionAttempts: 10,
+    forceNew: true,
+  }
+);
 export const SocketContext = React.createContext();
